8b76918c3f36844d41f569deef2bd4f2bcdd2e46
mc13mt49 <mc13mt49@linux-ac73.site>
2014-02-13 20:28:12 +0530

    see through done

--- src/SeeThroughImageApplet.java
+++ src/SeeThroughImageApplet.java
@@ -1,7 +1,11 @@
 
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
@@ -15,6 +19,9 @@ import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -43,6 +50,26 @@ class SeeThroughComponent extends Component{
         } catch (IOException ex) {
             Logger.getLogger(SeeThroughComponent.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        
+    }
+    
+        public Dimension getPreferredSize() {
+        return new Dimension(bi.getWidth(null), bi.getHeight(null));
+    }
+    
+    public void setOpacity(float opacity) {
+        scales[3] = opacity;
+        rop = new RescaleOp(scales, offsets, null);
+    }
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setColor(Color.white);
+        g2d.fillRect(0,0, getWidth(), getHeight());
+        g2d.setColor(Color.black);
+        g2d.setFont(new Font("Dialog", Font.BOLD, 24));
+        g2d.drawString("Java 2D is great!", 10, 80);
+        g2d.drawImage(bi, rop, 0, 0);
     }
 }
 
@@ -51,7 +78,7 @@ public class SeeThroughImageApplet extends JApplet{
     static String imageFileName = "duke_skateboard.jpg";
     private URL imageSrc;
  
-    public SeeThroughImageApplet () {
+    public SeeThroughImageApplet () {   
     }
  
     public SeeThroughImageApplet (URL imageSrc) {
@@ -59,7 +86,28 @@ public class SeeThroughImageApplet extends JApplet{
     }
     
     public void buildUI(){
-        
+        final SeeThroughComponent st = new SeeThroughComponent(imageSrc);
+        add("Center", st);
+        JSlider opacitySlider = new JSlider(0, 100);
+        opacitySlider.addChangeListener(new ChangeListener() {
+                public void stateChanged(ChangeEvent e) {
+                    JSlider slider = (JSlider)e.getSource();
+                    st.setOpacity(slider.getValue()/100f);
+                    st.repaint();
+                };
+            });
+        Dimension size = st.getPreferredSize();
+        Dimension sliderSize = opacitySlider.getPreferredSize();
+        resize(size.width, size.height+sliderSize.height);
+        add("South", opacitySlider);
+    }
+    
+    public void init() {
+        try {
+            imageSrc = new URL(getCodeBase(), imageFileName);
+        } catch (MalformedURLException e) {
+        }
+        buildUI();
     }
     
     public static void main(String[] args){
@@ -73,6 +121,10 @@ public class SeeThroughImageApplet extends JApplet{
         } catch (MalformedURLException e) {
         }
         SeeThroughImageApplet sta = new SeeThroughImageApplet(imageSrc);
+        sta.buildUI();
+        f.add("Center", sta);
+        f.pack();
+        f.setVisible(true);
     }
     
 }
